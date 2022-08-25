674f54cdc850699e6beedca7305c7a77716c6871
FXmario <fransiskus_mario87@outlook.com>
2022-08-25 21:34:44 +0700

    Using let to simplify code

--- spec/models/projects_spec.rb
+++ spec/models/projects_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Project do
+  let(:project) { Project.new }
+  let(:task) { Task.new }
+  
   it "consider a project with no task to be done" do
-    project = Project.new
     expect(project.done?).to be_truthy
   end
   
   it "knows that a project with an incomplete task is not done" do
-    project = Project.new
-    task = Task.new
     project.tasks << task
     expect(project.done?).to be_falsy
   end
