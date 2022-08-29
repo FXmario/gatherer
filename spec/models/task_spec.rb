bb585cffe54b8e0769c83994b5e8e35ec618ce4d
FXmario <fransiskus_mario87@outlook.com>
2022-08-30 00:51:36 +0700

    The First date

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,7 +8,28 @@ RSpec.describe Task do
   end
 
   it "allows us to complete a task" do
-    task.mark_complete
+    task.mark_completed
     expect(task).to be_complete
+  end
+
+  describe "velocity" do
+    let(:task) { Task.new(size: 3) }
+
+    it "does not count an incomplete task toward velocity" do
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
+
+    it "count a recently completed task toward velocity" do
+      task.mark_completed(1.day.ago)
+      expect(task).to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(3)
+    end
+
+    it "doesn't count a long-ago completed task toward velocity" do
+      task.mark_completed(6.months.ago)
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eq(0)
+    end
   end
 end
