58e17941087b0da254b30804a2f9ddcce7efc229
FXmario <fransiskus_mario87@outlook.com>
2022-09-27 16:27:19 +0700

    Finishing Ch.3

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 RSpec.describe "adding a project", type: :system do
+
    it "does not allow a user to create a project without a name" do
     visit new_project_path
     fill_in "Name", with: ""
@@ -20,4 +21,5 @@ RSpec.describe "adding a project", type: :system do
       expect(page).to have_selector(
          "#project_#{@project.id} .total-size", text: "8")
   end
+
 end
